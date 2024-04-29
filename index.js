9c9d2f96ef964e893c44254ae53eb6fb4e419421
Javier Andres Acevedo Gomez <gomezacevedoandresjavier0730@gmail.com>
2024-04-28 21:31:56 -0500

    Agregar funcionalidad de cantidad, sumar y restar JS

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ function ready() {
     var button = botonesEliminarItem[i];
     button.addEventListener("click", eliminarItemCarrito);
   }
+
+  // Agregar funcionalidad al boton sumar cantidad
+  var botonesSumarCantidad = document.getElementsByClassName("sumar-cantidad");
+  for (var i = 0; i < botonesSumarCantidad.length; i++) {
+    var button = botonesSumarCantidad[i];
+    button.addEventListener("click", sumarCantidad);
+  }
+
+  // Agregar funcionalidad al boton restar cantidad
+  var botonesRestarCantidad =
+    document.getElementsByClassName("restar-cantidad");
+  for (var i = 0; i < botonesRestarCantidad.length; i++) {
+    var button = botonesRestarCantidad[i];
+    button.addEventListener("click", restarCantidad);
+  }
+
+  // Agrego funcionalidad a los botones agregar al carrito
 }
 
 // Elimino el item seleccionado del carrito
@@ -25,7 +42,9 @@ function eliminarItemCarrito(event) {
   // Actualizar el total del carrito una vez eliminado el item
   actualizarTotalCarrito();
 
-  // La
+  // La siguiente funcion controla si hay elementos en el carrito una vez que se elimino
+  // si no lo hay se debe ocultar el carrito
+  ocultarCarrito();
 }
 
 // Actualiza el total del carrito
@@ -53,4 +72,51 @@ function actualizarTotalCarrito() {
   total = Math.round(total * 100) / 100;
   document.getElementsByClassName("carrito-precio-total")[0].innerText =
     "$" + total.toLocaleString("es");
+}
+
+function ocultarCarrito() {
+  var carritoItems = document.getElementsByClassName("carrito-items")[0];
+  if (carritoItems.childElementCount == 0) {
+    var carrito = document.getElementsByClassName("carrito")[0];
+    carrito.style.marginRight = "-100%";
+    carrito.style.opacity = "0";
+    carritoVisible = false;
+
+    // Maximiza el contenedor de los elementos
+    var items = document.getElementsByClassName("contenedor-items")[0];
+    items.style.width = "100%";
+  }
+}
+
+// Aumento en uno la cantidad del elemento seleccionado
+function sumarCantidad(event) {
+  var buttonClicked = event.target;
+  var selector = buttonClicked.parentElement;
+  var cantidadActual = selector.getElementsByClassName(
+    "carrito-item-cantidad"
+  )[0].value;
+  console.log(cantidadActual);
+  cantidadActual++;
+  selector.getElementsByClassName("carrito-item-cantidad")[0].value =
+    cantidadActual;
+  // Actualizar el total
+  actualizarTotalCarrito();
+}
+
+function restarCantidad(event) {
+  var buttonClicked = event.target;
+  var selector = buttonClicked.parentElement;
+  var cantidadActual = selector.getElementsByClassName(
+    "carrito-item-cantidad"
+  )[0].value;
+  console.log(cantidadActual);
+  cantidadActual--;
+
+  // Controlamos que no sea menor que 1
+  if (cantidadActual >= 1) {
+    selector.getElementsByClassName("carrito-item-cantidad")[0].value =
+      cantidadActual;
+    // Actualizar el total
+    actualizarTotalCarrito();
+  }
 }
